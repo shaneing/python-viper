f2e79047f4018ddc9cda25e339bb09780002de35
shaneing <z.shane.ing@gmail.com>
2020-01-16 00:11:59 +0800

    test: add testing for reading remote config

--- tests/test_viper.py
+++ tests/test_viper.py
@@ -1,13 +1,27 @@
 import os
 import unittest
 import viper
+from mock import patch
 
 
 def _absolute_path(p):
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), p)
 
 
+TEST_DATA = {
+    'LockIndex': 0,
+    'Key': 'hello',
+    'Flags': 0,
+    'Value': b'hello:\n  name: foo',
+    'CreateIndex': 51,
+    'ModifyIndex': 51
+}
+
+
 class TestViper(unittest.TestCase):
+    def setUp(self) -> None:
+        viper.reset()
+
     def test_bind_flags(self):
         @viper.bind_flags(flags=[
             viper.Flag(name='name', value='foo', usage='Indicate name'),
@@ -21,4 +35,13 @@ class TestViper(unittest.TestCase):
     def test_read_config(self):
         viper.set_config_path(_absolute_path('test.yml'))
         viper.read_config()
+        self.assertEqual('foo', viper.get('hello.name'))
+
+    @patch('requests.get')
+    def test_remote_config(self, mock_get):
+        mock_get.return_value.json.return_value = [TEST_DATA]
+        mock_get.return_value.status_code = 200
+        viper.set_config_type('yml')
+        viper.set_remote_provider('consul', '127.0.0.1', 8500, 'hello')
+        viper.read_remote_config()
         self.assertEqual('foo', viper.get('hello.name'))
